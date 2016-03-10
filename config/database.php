3c1b292310ff2056269ac530b00bd33bcfa4fd77
Matteo Loro <matteo.loro@gmail.com>
2016-03-10 18:22:39 +0100

    Removed sensible details

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'easy_agile'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'sidkorn81'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
